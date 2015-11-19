926fabedd9f8cd86863f888a0427c22f0c50377a
NotYourProblem <whynot@test.com>
2015-11-19 22:46:36 +0100

    ..

--- scripts/i3blocks/weather.sh
+++ scripts/i3blocks/weather.sh
@@ -2,4 +2,4 @@
 
 curl -s "http://rss.accuweather.com/rss/liveweather_rss.asp?metric=1&locCode=EUR|AT|AU007|INNSBRUCK" | perl -ne 'use utf8; if (/Currently/) {chomp;/\<title\>Currently: (.*)?\<\/title\>/; my @values=split(":",$1);
 binmode(STDOUT, ":utf8");
-print"$values[0] $values[1]"}'
+print"$values[0]$values[1]"}'
