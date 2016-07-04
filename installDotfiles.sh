b82d7d43e046f6b7f602bab25894831b6eb102c5
mike7549 <csap8796@student.uibk.ac.at>
2016-07-04 19:17:05 +0200

    changed some scripts

--- installDotfiles.sh
+++ installDotfiles.sh
@@ -13,7 +13,7 @@ ln -sf $dotfiles/zsh/.zshrc $homedir/.zshrc
 ln -sf $dotfiles/vim/init.vim $homedir/.vimrc
 ln -sf $dotfiles/xorg/.xinitrc $homedir/.xinitrc
 ln -sf $dotfiles/termite/config $homedir/.config/termite/config
-ln -sf $dotfiles/i3/config $homedir/.i3/config
+#ln -sf $dotfiles/i3/config $homedir/.i3/config
 ln -sf $dotfiles/i3/i3blocks.conf $homedir/.i3blocks.conf
 ln -sf $dotfiles/xorg/Xresources $homedir/.Xresources
 ln -sf $dotfiles/.bashrc $homedir/.bashrc
@@ -25,8 +25,10 @@ ln -sf $dotfiles/zsh/.zlogin $homedir/.zlogin
 #ln -sf $dotfiles/fish/functions/fish_prompt.fish $homedir/.config/fish/functions/fish_prompt.fish
 
 sudo touch /etc/NetworkManager/dispatcher.d/99-wlan
+touch $
 sudo ln -sf $dotfiles/scripts/wlan-on /etc/NetworkManager/dispatcher.d/99-wlan
 sudo ln -sf $dotfiles/ranger/rc.conf $rootdir/.config/ranger/rc.conf
+sudo ln -sf $dotfiles/ranger/rifle.conf $homedir/.config/ranger/rifle.conf
 sudo ln -sf $dotfiles/zsh/.zshrc $rootdir/.zshrc
 sudo ln -sf $dotfiles/vim/init.vim $rootdir/.vimrc
 sudo ln -sf $dotfiles/.resolv.conf	/etc/resolv.conf
