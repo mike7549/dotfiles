c57e9e9c94155717aa2405312b9dfeebbf0c9a0d
mike7549 <csap8796@student.uibk.ac.at>
2016-09-07 09:11:45 +0200

    updated installscript

--- installscript.sh
+++ installscript.sh
@@ -5,6 +5,7 @@ dotdir="/home/$USER/dotfiles"
 sudo pacman -Syu wget
 
 #pacaur
+gpg --recv-keys --keyserver hkp://pgp.mit.edu 1EB2638FF56C0C53
 git clone https://aur.archlinux.org/cower.git /tmp/cower
 git clone https://aur.archlinux.org/pacaur.git /tmp/pacaur
 cd /tmp/cower/
@@ -21,6 +22,7 @@ sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/to
 
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.config/oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 git clone git://github.com/zsh-users/zsh-autosuggestions $ZSH_CUSTOM/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-history-substring-search $ZSH_CUSTOM/plugins/zsh-history-substring-search
 
 rm ~/.zshrc
 
