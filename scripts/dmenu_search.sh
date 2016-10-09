c5177b7318c04c6f84173791c277516e59777e41
mike7549 <csap8796@student.uibk.ac.at>
2016-10-09 23:47:39 +0200

    updated dmenu search

--- scripts/dmenu_search.sh
+++ scripts/dmenu_search.sh
@@ -2,11 +2,14 @@
 
 if [[ $1 = "-g" ]]; then
 	URL='https://www.google.com/search?q='
+	QUERY=$(echo '' | dmenu -p "Google Search:" -sb '#a89984' -sf '#32302f' -fn "Hack-13" -b)
 elif [[ $1 = "-y" ]]; then
 	URL='https://www.youtube.com/results?search_query='
+	QUERY=$(echo '' | dmenu -p "YouTube Search:" -sb '#a89984' -sf '#32302f' -fn "Hack-13" -b)
 fi
 
-QUERY=$(echo '' | dmenu -p "Search:" -sb '#a89984' -sf '#32302f' -fn "Hack-13" -b)
+
+
 if [ -n "$QUERY" ]; then
 	xdg-open "${URL}${QUERY}" 2> /dev/null
 	exec i3-msg [class="^chromium$"] focus
