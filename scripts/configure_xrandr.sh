369738a480a605a053238bba16a42f708cf9ceba
mike7549 <csap8796@student.uibk.ac.at>
2016-12-05 09:23:33 +0100

    xrandr scripts changed

--- scripts/configure_xrandr.sh
+++ scripts/configure_xrandr.sh
@@ -1,14 +1,14 @@
 #! /bin/bash
 
 PRIMARY=$(xrandr | grep eDP | grep " connected" | awk '{print $1;}')
-EXT1=$(xrandr | grep HDMI | grep " connected" | awk '{print $1;}')
-EXT2=$(xrandr | grep DVI | grep " connected" | awk '{print $1;}')
+EXT1=$(xrandr | grep HDMI-0 | grep " connected" | awk '{print $1;}')
+EXT2=$(xrandr | grep HDMI-1 | grep " connected" | awk '{print $1;}')
 
 if ((xrandr | grep "$PRIMARY connected" && xrandr | grep "$EXT1 connected")); then
 	xrandr --output $PRIMARY --primary --auto --output $EXT1 --auto --right-of $PRIMARY
 	echo "$EXT1  enabled"
 elif (xrandr | grep "$EXT2 connected" && xrandr | grep "$EXT1 connected"); then
-	xrandr --output $EXT2 --primary --auto --output $EXT1 --auto --right-of $EXT2 
+	xrandr --output $EXT1 --primary --auto --output $EXT2 --auto --right-of $EXT1 
 	echo "$EXT2 and $EXT1 enabled"
 elif (xrandr | grep "$EXT1 disconnected" && xrandr | grep "$PRIMARY connected"); then
 	xrandr --output $EXT1 --off
