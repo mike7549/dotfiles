30b82feda7a14097ccb04a070599dfc97301c521
mike7549 <csap8796@student.uibk.ac.at>
2018-05-05 13:41:21 +0200

    switched to nerd fonts

--- installscript.sh
+++ installscript.sh
@@ -25,14 +25,16 @@ sudo nvim -u $dotdir/vim/init.vim -c :PlugInstall -c :q -c :q
 #other installations
 yes | sudo sensors-detect
 sudo pip install i3ipc
-sudo pip install fontawesome
+
+sudo cp -r $dotdir/Hack/ /usr/share/fonts
+sudo fc-cache
 
 sudo chsh -s /usr/bin/zsh
 chsh -s /usr/bin/zsh
 
 sh $dotdir/installDotfiles.sh
 
-sh -c "$(curl -fsSL https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/instll.sh)"
+sh -c "$(wget https://raw.githubusercontent.com/robbyrussell/oh-my-zsh/master/tools/install.sh -O -)"
 git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ~/.oh-my-zsh/custom/plugins/zsh-syntax-highlighting
 git clone https://github.com/zsh-users/zsh-autosuggestions ~/.oh-my-zsh/custom/plugins/zsh-autosuggestions
 git clone https://github.com/zsh-users/zsh-history-substring-search ~/.oh-my-zsh/custom/plugins/zsh-history-substring-search
