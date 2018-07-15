8ea6954d91de71863e8f69b8c3cfb42fbe2ca5df
mike7549 <csap8796@student.uibk.ac.at>
2018-07-15 10:38:49 +0200

    fixed some minor problems in some scripts

--- scripts/dmenu_search.sh
+++ scripts/dmenu_search.sh
@@ -2,14 +2,11 @@
 
 if [[ $1 = "-g" ]]; then
 	URL='https://www.google.com/search?q='
-	QUERY=$(echo '' | dmenu -p "Google Search:" -sb '#8fa1b3' -sf '#2b3030b' -fn "Hack-13" -b)
+	QUERY=$(echo '' | dmenu -p "Google Search:" -sb '#8fa1b3' -sf '#2b303b' -fn "Hack-13" -b)
 elif [[ $1 = "-y" ]]; then
 	URL='https://www.youtube.com/results?search_query='
 	QUERY=$(echo '' | dmenu -p "YouTube Search:" -sb '#8fa1b3' -sf '#2b303b' -fn "Hack-13" -b)
 fi
-
-
-
 
 if [ -n "$QUERY" ]; then
 	xdg-open "${URL}${QUERY}" 2> /dev/null
