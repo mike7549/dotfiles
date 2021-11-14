2405ee3dd6fe1992bfc3eabb6f0c5028b6506f77
mike7549 <jackhammer.aut@gmail.com>
2021-11-14 16:47:26 +0100

    minor updates

--- scripts/i3lock.sh
+++ scripts/i3lock.sh
@@ -1,8 +1,6 @@
 #!/bin/bash
-TMPBG=/tmp/screen.png
-LOCK=~/dotfiles/Pictures/lock.png
-RES=$(xrandr | grep 'current' | sed -E 's/.*current\s([0-9]+)\sx\s([0-9]+).*/\1x\2/')
+TMPBG=/tmp/screenlock.png
+RES=$(xrandr | grep 'Screen 0:' | awk '{print $8"x"$10}' | sed 's/.$//')
  
-ffmpeg -f x11grab -video_size $RES -y -i $DISPLAY -i $LOCK -filter_complex "boxblur=5:1,overlay=(main_w-overlay_w)/2:(main_h-overlay_h)/2" -vframes 1 $TMPBG -loglevel quiet
+ffmpeg -f x11grab -y -video_size $RES -i $DISPLAY -filter_complex "boxblur=5:1" -vframes 1 $TMPBG
 i3lock -i $TMPBG
-rm $TMPBG
