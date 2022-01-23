7347f81f90e9cdb772d3ba2de9defe01bd820ce3
mike7549 <csap8796@student.uibk.ac.at>
2022-01-23 21:56:23 +0100

    iwd, no block messages if no internet

--- scripts/createSymlinks.sh
+++ scripts/createSymlinks.sh
@@ -12,6 +12,7 @@ mkdir -p "$homedir/.config/gtk-3.0/"
 mkdir -p "$homedir/.config/rofi/"
 mkdir -p "$homedir/.config/kitty"
 sudo mkdir -p "/etc/X11/xorg.conf.d/" 
+sudo mkdir -p "/etc/iwd/"
 
 # create symlinks
 ln -sf $dotfiles/ranger/rc.conf $homedir/.config/ranger/rc.conf
@@ -34,7 +35,7 @@ sudo ln -sf $dotfiles/zsh/.zshrc $rootdir/.zshrc
 sudo ln -sf $dotfiles/vim/init.vim $rootdir/.vimrc
 sudo ln -sf $dotfiles/xorg.conf/00-keyboard.conf /etc/X11/xorg.conf.d/00-keyboard.conf
 sudo ln -sf $dotfiles/xorg.conf/20-nvidia.conf /etc/X11/xorg.conf.d/20-nvidia.conf
-
+sudo ln -sf $dotfiles/iwd/main.conf /etc/iwd/main.conf
 
 sudo sh -c "if [ -d $rootdir/dotfiles ]; then sudo rm -rf $rootdir/dotfiles; fi"
 sudo ln -s $dotfiles $rootdir/dotfiles
