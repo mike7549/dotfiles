c1cc3406399de60739178a96abf23c80e9eebbfb
mike7549 <jackhammer.aut@gmail.com>
2022-05-22 22:09:28 +0200

    ..

--- scripts/installscript.sh
+++ scripts/installscript.sh
@@ -9,12 +9,13 @@ sudo pacman -Syu wget base-devel
 git clone https://aur.archlinux.org/yay-bin.git
 cd yay-bin
 makepkg -si
+cd .. && rm -rf yay-bin
 
 # install all the dotfile dependecies
 yay -S $(cat $dotdir/scripts/packages)  --answerclean All --answerdiff All --answeredit All
 
 # nvim install
-curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 nvim -u $dotdir/vim/init.vim -c :PlugInstall -c :q -c :q
 sudo nvim -u $dotdir/vim/init.vim -c :PlugInstall -c :q -c :q
 
@@ -29,7 +30,7 @@ sudo fc-cache -f -v
 
 # oh-my-zsh
 sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-curl -L git.io/antigen > .antigen.zsh
+curl -L git.io/antigen > $HOME/.antigen.zsh
 
 sudo chsh -s /usr/bin/zsh
 chsh -s /usr/bin/zsh
@@ -41,4 +42,4 @@ flatpak install flathub -y com.google.Chrome com.visualstudio.code org.gnome.Tot
 sh $dotdir/scripts/createSymlinks.sh
 
 # change colors
-dircolors -p > ~/.dircolors
+dircolors -p > $HOME/.dircolors
