6698ca60a9f096e5462d30f28b1b4f83b050833a
mike7549 <jackhammer.aut@gmail.com>
2024-11-04 12:10:53 +0100

    i3 and installscript update

--- scripts/installscript.sh
+++ scripts/installscript.sh
@@ -2,37 +2,88 @@
 
 # set dotfiles directory
 dotdir="$HOME/dotfiles"
+configdir="$HOME/.config"
 # update System before starting
 sudo pacman -Syu wget base-devel
 
-# yay
-git clone https://aur.archlinux.org/yay-bin.git
-cd yay-bin
-makepkg -si
-cd .. && rm -rf yay-bin
+function install_yay {
+    echo "<< Installing yay >>"
+    git clone https://aur.archlinux.org/yay-bin.git
+    cd yay-bin
+    makepkg -si
+    cd .. && rm -rf yay-bin
+}
 
-# install all the dotfile dependecies
-yay -S $(cat $dotdir/scripts/packages)  --answerclean All --answerdiff All --answeredit All
+function install_packages {
+    echo "<< Installing packages >>"
+    yay -S $(cat $dotdir/scripts/packages)  --answerclean All --answerdiff All --answeredit All
+}
 
-# nvim install
-curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
-nvim -u $dotdir/vim/init.vim -c :PlugInstall -c :q -c :q
-sudo nvim -u $dotdir/vim/init.vim -c :PlugInstall -c :q -c :q
+function install_neovim {
+    echo "<< Installing neovim >>"
+    curl -fLo $HOME/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+    nvim -u $dotdir/config/vim/init.vim -c :PlugInstall -c :q -c :q
+    sudo nvim -u $dotdir/config/vim/init.vim -c :PlugInstall -c :q -c :q
+    mkdir -p "$configdir/nvim/"
+    ln -sf $dotdir/config/vim/init.vim $configdir/nvim/init.vim
+    ln -sf $dotdir/config/vim/init.vim $HOME/.vimrc
+}
+
+function install_zsh {
+    echo "<< Installing zsh and oh my zsh >>"
+    sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
+    curl -L git.io/antigen > $HOME/.antigen.zsh
+
+    sudo chsh -s /usr/bin/zsh
+    chsh -s /usr/bin/zsh
+
+    mkdir -p "$configdir/zsh/"
+    ln -sf $dotdir/config/zsh/zshrc $HOME/.zshrc
+    ln -sf $dotdir/config/zsh/zlogin $HOME/.zlogin
+}
+
+function create_symlinks {
+    echo "<< Creating symlinks >>"
+    # ranger
+    mkdir -p $configdir/ranger/
+    ln -sf $dotdir/config/ranger/rc.conf $configdir/ranger/rc.conf
+    ln -sf $dotdir/config/ranger/rifle.conf $configdir/ranger/rifle.conf
+
+    # rofi
+    mkdir -p $configdir/rofi/themes
+    ln -sf $dotdir/config/rofi/config.rasi $configdir/rofi/config.rasi
+    ln -sf $dotdir/config/rofi/themes/nord.rasi $configdir/rofi/themes/nord.rasi
+
+    # kitty
+    mkdir -p $configdir/kitty
+    ln -sf $dotdir/config/kitty/kitty.conf $configdir/kitty/kitty.conf
+    ln -sf $dotdir/config/kitty/nord.conf $configdir/kitty/nord.conf
+
+    mkdir -p $configdir/polybar
+    ln -sf $dotdir/config/polybar/config.ini $configdir/polybar/config.ini
+
+    mkdir -p $configdir/gtk-3.0/
+    ln -sf $dotdir/config/gtk/gtk.conf $configdir/gtk-3.0/settings.ini
+
+    sudo mkdir -p "/etc/X11/xorg.conf.d/"
+    sudo ln -sf $dotdir/config/xorg/keyboard.conf /etc/X11/xorg.conf.d/00-keyboard.conf
+
+    ln -sf $dotdir/config/picom/picom.conf $HOME/.picom.conf
+    ln -sf $dotdir/config/chrome/chrome-flags.conf $configdir/chrome-flags.conf
+    ln -sf $dotdir/config/xorg/xinitrc $HOME/.xinitrc
+}
+
+install_yay
+install_packages
+install_neovim
+install_zsh
+create_symlinks
 
 # detect sensors
 yes | sudo sensors-detect
 
+# build font cache
 sudo fc-cache -f -v
-
-# oh-my-zsh
-sh -c "$(wget https://raw.github.com/ohmyzsh/ohmyzsh/master/tools/install.sh -O -)"
-curl -L git.io/antigen > $HOME/.antigen.zsh
-
-sudo chsh -s /usr/bin/zsh
-chsh -s /usr/bin/zsh
-
-# create symlinks
-sh $dotdir/scripts/createSymlinks.sh
 
 # change colors
 dircolors -p > $HOME/.dircolors
