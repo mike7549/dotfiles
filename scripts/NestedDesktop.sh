27b251b036d7c9b469551a00ae38d7a2b000165d
mike7549 <michael.thurner.90@gmail.com>
2026-04-03 15:13:02 +0200

    add nested desktop script

---
+++ scripts/NestedDesktop.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+unset LD_PRELOAD
+unset XDG_DESKTOP_PORTAL_DIR
+unset XDG_SEAT_PATH
+unset XDG_SESSION_PATH
+RES=$(xdpyinfo | awk '/dimensions/{print $2}')
+
+mkdir $XDG_RUNTIME_DIR/nested_kde -p
+cat <<EOF > $XDG_RUNTIME_DIR/nested_kde/kwin_wayland_wrapper
+#!/bin/sh
+/usr/bin/kwin_wayland_wrapper \
+  --width $(echo "$RES" | cut -d 'x' -f 1) \
+  --height $(echo "$RES" | cut -d 'x' -f 2) \
+  --no-lockscreen \
+  \$@
+EOF
+chmod a+x $XDG_RUNTIME_DIR/nested_kde/kwin_wayland_wrapper
+export PATH=$XDG_RUNTIME_DIR/nested_kde:$PATH
+
+dbus-run-session startplasma-wayland &
+PLASMA_PID=$!
+
+# Wait for KWin to be ready, then apply focus settings via dbus
+sleep 5
+KWIN_DBUS=$(dbus-send --session --print-reply --dest=org.kde.KWin /KWin org.kde.KWin.supportInformation 2>/dev/null)
+if [ -n "$KWIN_DBUS" ]; then
+  dbus-send --session --dest=org.kde.KWin /KWin org.kde.KWin.reconfigure
+fi
+
+wait $PLASMA_PID
+rm -f $XDG_RUNTIME_DIR/nested_kde/kwin_wayland_wrapper
